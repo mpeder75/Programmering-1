cdc32f27609a946e6078291313804660b1e392ef
Michael <mpeder75@gmail.com>
2023-09-02 16:03:34 +0200

    Add project files.

---
+++ Session 1 - Exercise 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_1___Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
