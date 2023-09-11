ad3f14f3ba7886cb3d808868b4f4ececc85207c1
Mpeder <mpeder75@gmail.com>
2023-09-11 08:44:19 +0200

    updat local repository

--- Session 2 - Exercise 3 + 4/Program.cs
+++ Session 2 - Exercise 3 + 4/Program.cs
@@ -8,11 +8,10 @@ Console.WriteLine($"Længde af array : {myIntArray.Length} indexes");
 
 
 
-
 // -------------------------------------------------------------------------- //
 
 /* Opgave 4: Læg tallene på indeks 1,3,5,8 sammen */
 
 int sum = myIntArray[1] + myIntArray[3] + myIntArray[5] + myIntArray[8];
-Console.WriteLine($"\nSummen af index {myIntArray.GetValue(0)}, {myIntArray.GetValue(2)}, {myIntArray.GetValue(4)}, {myIntArray.GetValue(7)} er : {sum}");
-Console.WriteLine($"\nSummen af int værdier i array : {myIntArray.Sum()}");
+Console.WriteLine($"\nSummen af værdier i index {myIntArray.GetValue(0)}, {myIntArray.GetValue(2)}, {myIntArray.GetValue(4)}, {myIntArray.GetValue(7)} = {sum}");
+Console.WriteLine($"\nSummen af int værdier i hele array = {myIntArray.Sum()}");
