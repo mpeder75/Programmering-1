fb5647ba694445bd13c9d22da136a140a46629c3
Mpeder <mpeder75@gmail.com>
2023-10-03 21:44:47 +0200

    minor refactoring + Sessoin 7 - Microsoft Learn Constructors

---
+++ Session 7 - Microsoft Learn - Constructors/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+// 1. man istanciere myCar udfra Car class
+// NÅR Car() bruges kaldes constructor, her uden at sætte værdier
+Car myCar = new Car();
+
+
+// 2. Nu bruges den overloaded constructor med argument
+Car myNewCar = new Car("Audi", "A6", 2020, "Dark Blue");
+
+// 3. myNewCar printes
+Console.WriteLine($"{myNewCar.Make} {myNewCar.Model} {myNewCar.Year} {myNewCar.Color}");
+
+
+
+
+class Car
+{
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public string Color { get; set; }
+    public double OriginalPrice { get; set; }
+
+
+    //todo Constructor - Her hardcodes fields igennem constructor
+    public Car()
+    {
+        this.Make = "Nissan";
+    }
+
+    //todo Overload constructor
+    public Car(string make, string model, int year, string color)
+    {
+        Make = make;
+        Model = model;
+        Year = year;
+        Color = color;
+    }
+}
