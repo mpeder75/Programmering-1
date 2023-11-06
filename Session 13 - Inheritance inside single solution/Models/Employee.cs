b48968293c5d3fcacd896a198840bb1f668f6784
Mpeder <mpeder75@gmail.com>
2023-11-06 18:22:57 +0100

    session 13 - Exercise del1

---
+++ Session 13 - Inheritance inside single solution/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_13___Inheritance_inside_single_solution.Models
+{
+    public class Employee : Contact
+    {
+        public int EmployeeId { get; set; }
+        public int SalaryLevel { get; set; }
+        public string Title { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}\nFirstname: {FirstName}\nLastname: {LastName}\nAddress: {Address}\nCity: {City}\nPhinenumber: {PhoneNumber}\nEmployeeId: {EmployeeId}\nSalarylevel: {SalaryLevel}\nTitle: {Title}";
+        }
+    }
+}
---
+++ Session 13 - Inheritance inside single solution/Program.cs
@@ -0,0 +1,5 @@
+﻿using Session_13___Inheritance_inside_single_solution.Models;
+
+
+Employee abe = new Employee { Id = 1, EmployeeId = 10, FirstName = "Abe", LastName = "Simpson", Address = "Terrance Avenue", City = "Springfield", PhoneNumber = 12345678, SalaryLevel = 10, Title = "Worker"};
+Console.WriteLine(abe.ToString());
